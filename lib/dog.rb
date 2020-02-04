cd0f4c333a4ecb7cc172fa0a922b466cae5dd77b
Danielle Torres <cruz_danielle_torres@yahoo.com>
2020-02-04 00:02:54 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,25 +1,21 @@
 class Dog
   def initialize(name, breed)
     @name = name
+    @breed = breed
   end
  
   def name=(name)
     @name = name
+    breed=(breed)
+    @breed = breed
   end
  
   def name
     @name
   end
   
-  def initialize(breed)
-    @breed = breed 
-  end 
-  
-  def breed=(breed)
-    @breed = breed 
-  end 
-  
   def breed 
     @breed 
   end
+  
 end
